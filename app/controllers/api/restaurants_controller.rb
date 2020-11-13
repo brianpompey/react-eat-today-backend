6f3e1444263e21b1fc1f9fb160dd69f9f7de3788
brianpompey <brianthescholar@gmail.com>
2020-11-13 08:29:24 -0500

    figuring out api

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,3 +1,30 @@
 class Api::V1::RestaurantsController < ApplicationController
+
+    def index
+        restaurants = Restaurant.all
+
+        render json: RestaurantSerializer.new(restaurants).serialized_json
+    end
+
+    def show
+        restaurant = Restaurant.find(params[:id])
+
+        render json: RestaurantSerializer.new(restaurant).serialized_json
+    end
     
+    def create
+        restaurant = Restaurant.new(restaurant_params)
+
+        if @restaurant.save
+            render json: RestaurantSerializer.new(restaurant).serialized_json
+        else
+            render json: { error: airline.errors.messages }, status: 422
+        end
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :location, :cuisine, :search)
+    end
 end
